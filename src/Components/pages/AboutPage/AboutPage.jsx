59cc2368d9ff2ac4ffcd1b8e45274152227e86f0
Sofía Viotti <sofi_04_04@hotmail.com>
2024-01-14 12:49:20 -0300

    About u

--- src/Components/pages/AboutPage/AboutPage.jsx
+++ src/Components/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const AboutPage = () => {
-return (
-    <div>About Us page</div>
-)
-}
 
+    useEffect(() => {
+
+        const clickear = () => {
+            console.log("click");
+        }
+
+    window.addEventListener("click", () => {
+        console.log("click")
+    })
+    return () => {
+        window.removeEventListener("click", () => {
+            console.log("click")
+        })
+    }
+    }, [])
+return (
+    <div className='container'>
+        <h1 className='main-title'>About Nati</h1>
+        <p>Este es el componente Sobre mi</p>
+    </div>
+    )
+}
 export default AboutPage
